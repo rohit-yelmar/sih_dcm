83ad673489a735b9ed6fc0570225fb7deea4bdff
tanayvaswani <vaswani.tanay9@gmail.com>
2023-09-18 21:06:30 +0530

    Notification section added

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import {Box,Divider,Drawer,IconButton,List,ListItem,ListItemButton,ListItemIcon,ListItemText,Typography,useTheme} from '@mui/material'
+import React from "react";
 import {
-    SettingsOutlined,
-    ChevronLeft,
-    ChevronRightOutlined,
-    HomeOutlined,
-    ShoppingCartOutlined,
-    Groups2Outlined,
-    ReceiptLongOutlined,
-    PublicOutlined,
-    PointOfSaleOutlined,
-    TodayOutlined,
-    CalendarMonthOutlined,
-    AdminPanelSettingsOutlined,
-    TrendingUpOutlined,
-    PieChartOutlined,
-  } from "@mui/icons-material";
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  PointOfSaleOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutlined,
+} from "@mui/icons-material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
@@ -23,134 +37,177 @@ import profileImage from "assets/profile.png";
 
 //Nav Items to Map Through
 const navItems = [
-    {
-        text:"Dashboard",
-        icon:<HomeOutlined />
-    },
-    {
-        text:"CaseFilling",
-        icon:<HomeOutlined />
-    },
-    {
-        text:"Overview",
-        icon:<TrendingUpOutlined />
-    },
-    {
-        text:"Calendar",
-        icon:<CalendarMonthOutlined />
-    },
-    {
-        text:"CaseFlow",
-        icon:<ReceiptLongOutlined /> 
-    },
-    {
-        text:"Profile",
-        icon:<Groups2Outlined />
-    },
-    {
-        text:"SignOut",
-        icon:<HomeOutlined />
-    },
-    {
-        text:"Meetings",
-        icon:<TodayOutlined />
-    },
-    {
-        text:"TextEditor",
-        icon:<TodayOutlined />
-    }
-]
+  {
+    text: "Dashboard",
+    icon: <HomeOutlined />,
+  },
+  {
+    text: "CaseFilling",
+    icon: <HomeOutlined />,
+  },
+  {
+    text: "Overview",
+    icon: <TrendingUpOutlined />,
+  },
+  {
+    text: "Calendar",
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: "CaseFlow",
+    icon: <ReceiptLongOutlined />,
+  },
+  {
+    text: "Profile",
+    icon: <Groups2Outlined />,
+  },
+  {
+    text: "Meetings",
+    icon: <TodayOutlined />,
+  },
+  {
+    text: "TextEditor",
+    icon: <TodayOutlined />,
+  },
+  {
+    text: "Notifications",
+    icon: <NotificationsIcon />,
+  },
+  {
+    text: "Log Out",
+    icon: <LogoutIcon />,
+  },
+];
 
-const Sidebar = ({user,drawerWidth,isSidebarOpen,setIsSidebarOpen,isNonMobile,}) => {
-    const {pathname} = useLocation();
-    const [active,setActive] = useState("");
-    const navigate = useNavigate()
-    const theme = useTheme()
-    useEffect(() =>{
-        setActive(pathname.substring(1))
-    },[pathname])
+const Sidebar = ({
+  user,
+  drawerWidth,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
+}) => {
+  const { pathname } = useLocation();
+  const [active, setActive] = useState("");
+  const navigate = useNavigate();
+  const theme = useTheme();
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
   return (
     <Box component="nav">
-        <Drawer
-          open={isSidebarOpen}
-          onClose={() => setIsSidebarOpen(false)}
-          variant="persistent"
-          anchor="left"
-          sx={{
+      <Drawer
+        open={isSidebarOpen}
+        onClose={() => setIsSidebarOpen(false)}
+        variant="persistent"
+        anchor="left"
+        sx={{
+          width: drawerWidth,
+          "& .MuiDrawer-paper": {
+            color: theme.palette.secondary[200],
+            backgroundColor: theme.palette.background.alt,
+            boxSixing: "border-box",
+            borderWidth: isNonMobile ? 0 : "2px",
             width: drawerWidth,
-            "& .MuiDrawer-paper": {
-              color: theme.palette.secondary[200],
-              backgroundColor: theme.palette.background.alt,
-              boxSixing: "border-box",
-              borderWidth: isNonMobile ? 0 : "2px",
-              width: drawerWidth,
-            },
-          }}
-        >
-            <Box width="100%">
-                <Box m="1.5rem 2rem 2rem 3rem">
-                    <FlexBetween color={theme.palette.secondary.main}>
-                        <Box display="flex" alignItems="center" gap="0.5rem" >
-                            <Typography variant='h4' fontWeight="bold">
-                                Distributed Case Management
-                            </Typography>
-
-                        </Box>
-                        {!isNonMobile && (
-                            <IconButton onClick={()=>setIsSidebarOpen(!isSidebarOpen)}>
-                                <ChevronLeft />
-                            </IconButton>
-                        )}
-                    </FlexBetween>
-                </Box>
-                <List>
-                    {navItems.map(({ text, icon }) => {
-                        const lcText = text.toLowerCase();
-                        return(
-                            <ListItem key={text} disablePadding >
-                                <ListItemButton onClick={()=>{
-                                    navigate(`/${lcText}`);
-                                    setActive(lcText);
-
-                                    }}
-                                    sx={{backgroundColor:active===lcText ? theme.palette.secondary[300]:"transparent",color:active===lcText ? theme.palette.primary[600]:theme.palette.secondary[100]}}
-                                    >
-                                        <ListItemIcon sx={{ml:"2rem",color:active===lcText ? theme.palette.primary[600]:theme.palette.secondary[200],}} >
-                                            {icon}
-                                        </ListItemIcon>
-                                        <ListItemText primary={text} />
-                                        {active === lcText && (
-                                            <ChevronRightOutlined sx={{ ml: "auto" }} />
-                                        )}
-                                        
-
-                                    </ListItemButton>
-                            </ListItem>
-                        )
-                    })}
-                </List>
+          },
+        }}
+      >
+        <Box width="100%">
+          <Box m="1.5rem 2rem 2rem 3rem">
+            <FlexBetween color={theme.palette.secondary.main}>
+              <Box display="flex" alignItems="center" gap="0.5rem">
+                <Typography variant="h4" fontWeight="bold">
+                  Distributed Case Management
+                </Typography>
+              </Box>
+              {!isNonMobile && (
+                <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                  <ChevronLeft />
+                </IconButton>
+              )}
+            </FlexBetween>
+          </Box>
+          <List>
+            {navItems.map(({ text, icon }) => {
+              const lcText = text.toLowerCase();
+              return (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton
+                    onClick={() => {
+                      navigate(`/${lcText}`);
+                      setActive(lcText);
+                    }}
+                    sx={{
+                      backgroundColor:
+                        active === lcText
+                          ? theme.palette.secondary[300]
+                          : "transparent",
+                      color:
+                        active === lcText
+                          ? theme.palette.primary[600]
+                          : theme.palette.secondary[100],
+                    }}
+                  >
+                    <ListItemIcon
+                      sx={{
+                        ml: "2rem",
+                        color:
+                          active === lcText
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[200],
+                      }}
+                    >
+                      {icon}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                    {active === lcText && (
+                      <ChevronRightOutlined sx={{ ml: "auto" }} />
+                    )}
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Box>
+        <Box position="absolute" bottom="2rem">
+          <Divider />
+          <FlexBetween
+            textTransform="none"
+            gap="1rem"
+            m="1.5rem 2rem 0rem 3rem"
+          >
+            <Box
+              component="img"
+              alt="profile"
+              src={profileImage}
+              height="40px"
+              width="40px"
+              borderRadius="50%"
+              sx={{ objectFit: "cover" }}
+            />
+            <Box textAlign="left">
+              <Typography
+                fontWeight="bold"
+                fontSize="0.9rem"
+                sx={{ color: theme.palette.secondary[100] }}
+              >
+                {user.name}
+              </Typography>
+              <Typography
+                fontWeight="bold"
+                fontSize="0.9rem"
+                sx={{ color: theme.palette.secondary[200] }}
+              >
+                {user.occupation}
+              </Typography>
             </Box>
-            <Box position="absolute" bottom="2rem" >
-                <Divider />
-                <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0rem 3rem" >
-                    <Box component="img" alt="profile" src={profileImage} height="40px" width="40px" borderRadius="50%" sx={{objectFit:"cover"}} />
-                        <Box textAlign="left" >
-                            <Typography fontWeight="bold" fontSize="0.9rem" sx={{color:theme.palette.secondary[100]}}>
-                                {user.name}
-                            </Typography>
-                            <Typography fontWeight="bold" fontSize="0.9rem" sx={{color:theme.palette.secondary[200]}}>
-                                {user.occupation}
-                            </Typography>
-
-                        </Box>
-                        <SettingsOutlined sx={{color:theme.palette.secondary[300],fontSize:"25px"}} />
-                    
-                </FlexBetween>
-
-            </Box>
-        </Drawer>
+            <SettingsOutlined
+              sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+            />
+          </FlexBetween>
+        </Box>
+      </Drawer>
     </Box>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
---
+++ client/src/scenes/notifications/index.jsx
@@ -0,0 +1,41 @@
+import { Alert, AlertTitle } from "@mui/material";
+import React from "react";
+
+const Notifications = () => {
+  return (
+    <div>
+      <Alert severity="info">
+        <AlertTitle>Case Update</AlertTitle>
+        Hello user! There might be some new updates regarding the case you've filed — <strong>check it out!</strong>
+      </Alert>
+      <br />
+      <Alert severity="info">
+        <AlertTitle>Case Update</AlertTitle>
+        Hello user! There might be some new updates regarding the case you've filed — <strong>check it out!</strong>
+      </Alert>
+      <br />
+      <Alert severity="info">
+        <AlertTitle>Case Update</AlertTitle>
+        Hello user! There might be some new updates regarding the case you've filed — <strong>check it out!</strong>
+      </Alert>
+      <br />
+      <Alert severity="info">
+        <AlertTitle>Case Update</AlertTitle>
+        Hello user! There might be some new updates regarding the case you've filed — <strong>check it out!</strong>
+      </Alert>
+      <br />
+      <Alert severity="info">
+        <AlertTitle>Case Update</AlertTitle>
+        Hello user! There might be some new updates regarding the case you've filed — <strong>check it out!</strong>
+      </Alert>
+      <br />
+      <Alert severity="info">
+        <AlertTitle>Case Update</AlertTitle>
+        Hello user! There might be some new updates regarding the case you've filed — <strong>check it out!</strong>
+      </Alert>
+      <br />
+    </div>
+  );
+};
+
+export default Notifications;
